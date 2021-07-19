53d3de6a3155f98d54024c057d63d2a48581a84e
DomenicoCentrone <76876906+DomenicoCentrone@users.noreply.github.com>
2021-07-19 10:21:41 +0200

    .

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,63 @@
 <?php
-if (isset($_POST["submit"])) {
-  // Checking For Blank Fields..
-  if ($_POST["name"] == "" || $_POST["email"] == "" || $_POST["subject"] == "" || $_POST["message"] == "") {
-    echo "Fill All Fields..";
-  } else {
-    // Check if the "Sender's Email" input field is filled out
-    $email = $_POST['email'];
-    // Sanitize E-mail Address
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    // Validate E-mail Address
-    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    if (!$email) {
-      echo "Invalid Sender's Email";
-    } else {
-      $subject = $_POST['subject'];
-      $message = $_POST['message'];
-      $headers = 'From:' . $email2 . "rn"; // Sender's Email
-      $headers .= 'Cc:' . $email2 . "rn"; // Carbon copy to Sender
-      // Message lines should not exceed 70 characters (PHP rule), so wrap it
-      $message = wordwrap($message, 70);
-      // Send Mail By PHP Mail Function
-      mail("domenicocentrone22@gmail.com", $subject, $message, $headers);
-      echo "Your mail has been sent successfuly ! Thank you for your feedback";
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name']; // Get Name value from HTML Form
+        $email_id = $_POST['email']; // Get Email Value
+        $mobile_no = $_POST['mobile']; // Get Mobile No
+        $msg = $_POST['message']; // Get Message Value
+         
+        $to = "abc@gmail.com"; // You can change here your Email
+        $subject = "'$name' has been sent a mail"; // This is your subject
+         
+        // HTML Message Starts here
+        $message ="
+        <html>
+            <body>
+                <table style='width:600px;'>
+                    <tbody>
+                        <tr>
+                            <td style='width:150px'><strong>Name: </strong></td>
+                            <td style='width:400px'>$name</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Email ID: </strong></td>
+                            <td style='width:400px'>$email_id</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Mobile No: </strong></td>
+                            <td style='width:400px'>$mobile_no</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Message: </strong></td>
+                            <td style='width:400px'>$msg</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+        </html>
+        ";
+        // HTML Message Ends here
+         
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+ 
+        // More headers
+        $headers .= 'From: Admin <admin@websapex.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
+        $headers .= 'Cc: info@websapex.com' . "\r\n"; // If you want add cc
+        $headers .= 'Bcc: boss@websapex.com' . "\r\n"; // If you want add Bcc
+         
+        if(mail($to,$subject,$message,$headers)){
+            // Message if mail has been sent
+            echo "<script>
+                    alert('Mail has been sent Successfully.');
+                </script>";
+        }
+ 
+        else{
+            // Message if mail has been not sent
+            echo "<script>
+                    alert('EMAIL FAILED');
+                </script>";
+        }
     }
-  }
-}
